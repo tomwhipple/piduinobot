619a273c63f50786c5fb1fc01036ca41044e0702
Tom Whipple <mail@tomwhipple.com>
2016-09-11 16:11:01 -0700

    oops

--- src/RobotState.c
+++ src/RobotState.c
@@ -4,7 +4,7 @@
 
 void initalizeRobotState(robot_state_t* state) {
   state->distance = 0;
-
+}
 
 void getRobotStateString(char** buf, robot_state_t* state) {
   sprintf(buf, "{\"distance\":%i},", state->distance);
